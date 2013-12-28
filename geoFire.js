cd014e115948f726065d5e15f081b3a5025ed202
bennlich@gmail.com <bennlich@gmail.com>
2013-12-27 21:41:47 -0700

    Added invalid id error condition for geoFire.removeById.

--- geoFire.js
+++ geoFire.js
@@ -318,12 +318,17 @@
         this._agents.child(id).once('value', 
                                     function (snapshot) {
                                         var data = snapshot.val();
-                                        self._firebase.child(data.geohash).child(id).remove(function(error) {
-                                                if (!error)
-                                                    self._agents.child(id).remove(cb);                                                    
-                                                else
-                                                    cb(error);
-                                            });
+                                        if (data === null) {
+                                            cb(new Error("geoFire.removeById error: Invalid Id argument."));
+                                        }
+                                        else {
+                                            self._firebase.child(data.geohash).child(id).remove(function(error) {
+                                                    if (!error)
+                                                        self._agents.child(id).remove(cb);                                                    
+                                                    else
+                                                        cb(error);
+                                                });    
+                                        }
                                     });
     };
 
