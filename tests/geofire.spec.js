10216f9122c3daaa01846eedc11b0deb89c56f6b
Andrew Lee <andrew@firebase.com>
2014-04-22 13:16:05 -0700

    Added promises to get and set and added one test case.

--- tests/geofire.spec.js
+++ tests/geofire.spec.js
@@ -1,5 +1,35 @@
 describe("GeoFire Tests", function() {
-  it("stub", function() {
-    expect(true).toBe(true);
-  });
+
+  function Checklist(items, doneCB) {
+    var eventsToComplete = items ? items : ["default"];
+
+    this.x = function(item) {
+      var ind = eventsToComplete.indexOf(item);
+      if(ind >= 0) {
+        eventsToComplete.splice(ind, 1);
+      }
+      if(eventsToComplete.length == 0) {
+        doneCB();
+      }
+    }
+  }
+
+  var ref = new Firebase('https://geofiretest.firebaseio-demo.com');
+
+  it("get and set return promises", function(done) {
+    var cl = new Checklist(["first promise", "second promise"], done);
+
+    var gf = new GeoFire(ref);
+    var p1 = gf.set("hello", [1,2]);
+    var p2 = gf.get("hello");
+
+    p1.then(function() {
+      cl.x("first promise");
+    });
+
+    p2.then(function(loc) {
+      expect(loc).toEqual([1,2]);
+      cl.x("second promise");
+    });
+  })
 });
