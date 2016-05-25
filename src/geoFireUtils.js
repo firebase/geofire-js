66292a95a5210b46d72fbc5b588f8c35cfcad665
Jacob Wenger <jacob@firebase.com>
2016-05-25 13:48:51 -0700

    Fixed JSDoc comment

--- src/geoFireUtils.js
+++ src/geoFireUtils.js
@@ -453,7 +453,7 @@ function decodeGeoFireObject(geoFireObj) {
  * Returns the key of a Firebase snapshot across SDK versions.
  *
  * @param {DataSnapshot} snapshot A Firebase snapshot.
- * @return {String|null} key The Firebase snapshot's key.
+ * @return {string|null} key The Firebase snapshot's key.
  */
  function getKey(snapshot) {
    var key;
