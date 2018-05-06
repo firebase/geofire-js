6b7ca8a57d1e336f4fc0c4c9f295f805705c8ada
Michael Solati <mkslt04@gmail.com>
2018-05-06 13:39:29 -0700

    build: migrate build process to rollup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export { GeoCallbackRegistration } from './geoFire/geoCallbackRegistration';
 export { GeoFire } from './geoFire';
+export { GeoQuery } from './geoFire/geoQuery';
 export * from './geoFire/interfaces';
