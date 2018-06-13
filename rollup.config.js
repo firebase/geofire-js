2859aa8192ac514d2ce90604a17b3ff7bdbb3923
Michael Solati <mkslt04@gmail.com>
2018-06-13 07:31:23 -0700

    chore: update dependencies and support firebase 5.x.x

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolveModule from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 const GLOBAL_NAME = 'geofire';
