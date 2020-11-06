45a1ac4e3cfb9f231a857d5821fe9fe10b19cbed
Frank van Puffelen <frank.vanpuffelen@gmail.com>
2020-11-06 07:32:56 -0800

    Re-enable dummy test
    Since having no tests seems to make the unit tests fail

--- packages/geofire-common/test/Main.test.ts
+++ packages/geofire-common/test/Main.test.ts
@@ -2,8 +2,9 @@ import * as chai from 'chai';
 
 const expect = chai.expect;
 
-// describe('GeoFire GeoCallbackRegistration Tests:', () => {
-//   it("hello world", () => {
-//     expect(true).to.be.true;
-//   });
-// });
+// TODO: move relevant tests from the `geofire` package to `geofire-common`.
+describe('Dummy test to ensure the run succeeds:', () => {
+  it("hello world", () => {
+    expect(true).to.be.true;
+  });
+});
