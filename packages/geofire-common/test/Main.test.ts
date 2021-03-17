4c49564b641c75e9543ca7c443570e77cd7ec02e
Frank van Puffelen <puf@google.com>
2021-03-16 17:03:20 -0700

    Move geohashQuery tests to GeoFireUtils

--- packages/geofire-common/test/Main.test.ts
+++ packages/geofire-common/test/Main.test.ts
@@ -228,3 +228,41 @@ describe('geoFireUtils Tests:', () => {
     });
   });
 });
+
+describe('Geohash queries:', () => {
+  it('Geohash queries must be of the right size', () => {
+    expect(geohashQuery('64m9yn96mx', 6)).to.be.deep.equal(['60', '6h']);
+    expect(geohashQuery('64m9yn96mx', 1)).to.be.deep.equal(['0', 'h']);
+    expect(geohashQuery('64m9yn96mx', 10)).to.be.deep.equal(['64', '65']);
+    expect(geohashQuery('6409yn96mx', 11)).to.be.deep.equal(['640', '64h']);
+    expect(geohashQuery('64m9yn96mx', 11)).to.be.deep.equal(['64h', '64~']);
+    expect(geohashQuery('6', 10)).to.be.deep.equal(['6', '6~']);
+    expect(geohashQuery('64z178', 12)).to.be.deep.equal(['64s', '64~']);
+    expect(geohashQuery('64z178', 15)).to.be.deep.equal(['64z', '64~']);
+  });
+
+  it('Query bounds from geohashQueryBounds must contain points in circle', () => {
+    function inQuery(queries, hash) {
+      for (let i = 0; i < queries.length; i++) {
+        if (hash >= queries[i][0] && hash < queries[i][1]) {
+          return true;
+        }
+      }
+      return false;
+    }
+    for (let i = 0; i < 200; i++) {
+      const centerLat = Math.pow(Math.random(), 5) * 160 - 80;
+      const centerLong = Math.pow(Math.random(), 5) * 360 - 180;
+      const radius = Math.random() * Math.random() * 100000;
+      const degreeRadius = metersToLongitudeDegrees(radius, centerLat);
+      const queries = geohashQueryBounds([centerLat, centerLong], radius);
+      for (let j = 0; j < 1000; j++) {
+        const pointLat = Math.max(-89.9, Math.min(89.9, centerLat + Math.random() * degreeRadius));
+        const pointLong = wrapLongitude(centerLong + Math.random() * degreeRadius);
+        if (distanceBetween([centerLat, centerLong], [pointLat, pointLong]) < radius / 1000) {
+          expect(inQuery(queries, geohashForLocation([pointLat, pointLong]))).to.be.equal(true);
+        }
+      }
+    }
+  });
+});
